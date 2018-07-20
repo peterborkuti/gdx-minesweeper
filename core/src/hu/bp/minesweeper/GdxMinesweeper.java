fb40e202f0878c7697c140955b89f2c099f81056
Peter Borkuti <peter@aspire>
2018-07-20 20:52:30 +0200

    stage

--- core/src/hu/bp/minesweeper/GdxMinesweeper.java
+++ core/src/hu/bp/minesweeper/GdxMinesweeper.java
@@ -2,32 +2,36 @@ package hu.bp.minesweeper;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import hu.bp.minesweeper.core.Minesweeper;
+import hu.bp.minesweeper.core.MinesweeperData;
 
 public class GdxMinesweeper extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
+	private Stage stage;
+
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		stage = new Stage(new ScreenViewport());
+		Gdx.input.setInputProcessor(stage);
 	}
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+	public void resize (int width, int height) {
+		// See below for what true means.
+		stage.getViewport().update(width, height, true);
 	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+
+	public void render (float delta) {
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		stage.act(delta);
+		stage.draw();
+	}
+
+	public void dispose() {
+		stage.dispose();
 	}
 }
