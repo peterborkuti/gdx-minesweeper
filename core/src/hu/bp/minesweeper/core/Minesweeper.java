c47281384b3c0fe8e2c1675de923c8a85bc4a973
Peter Borkuti <peter@aspire>
2018-07-28 22:22:20 +0200

    more doc, another uniqrandom algorithm, more tests

--- core/src/hu/bp/minesweeper/core/Minesweeper.java
+++ core/src/hu/bp/minesweeper/core/Minesweeper.java
@@ -1,9 +1,7 @@
 package hu.bp.minesweeper.core;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -36,7 +34,21 @@ public class Minesweeper {
 	private Minesweeper() {}
 
 	/**
-	 * Creates minesweeper data based on the size of the grid and the desired number of bombs
+	 * Creates minesweeper data based on the size of the grid and the desired number of bombs.
+	 *
+	 * This is for "end users". So use this when you wnat to generate a filled-in minesweeper grid.
+	 *
+	 * Example: 10x10 grid with 20 bombs
+	 * MinesweeperData data = Minesweeper.createMinesweeperData(10, 10, 20);
+	 * String grid = Minesweeper.getStringGrid(data);
+	 * System.out.println(grid);
+	 *
+	 * Example: 7x4 grid with 5 bombs
+	 * System.out.println(Minesweeper.getStringGrid(Minesweeper.createMinesweeperData(7, 4, 5)));
+	 *
+	 * Example: 6x5 grid with 4 bombs and Integer[][] gridmatrix
+	 * Integer[][] grid = Minesweeper.getStringGrid(Minesweeper.createMinesweeperData(6, 5, 4));
+	 *
 	 * @param rows
 	 * @param cols
 	 * @param numberOfBombs
@@ -50,7 +62,7 @@ public class Minesweeper {
 
 	/**
 	 * Creates a minesweeper datat based on the size of the grid and the linear coordinates of the bombs
-	 * It is useful for testing
+	 * It is useful for testing. End users may do not want to use this method.
 	 * @param rows
 	 * @param cols
 	 * @param bombs
@@ -66,6 +78,16 @@ public class Minesweeper {
 	 * Generates "length" pieces of unique random numbers which are from between 0 and bound - 1.
 	 * Unique means, that the number will not be repeated.
 	 *
+	 * This method uses this algorithm:
+	 * - generating numbers from 0 to bound -1
+	 * - stands them in random order
+	 * - selects the first "length" pieces.
+	 *
+	 * I know a better algorithm which takes exactly bound steps:
+	 * for ( i = 0; i < bound; i++) {
+	 *
+	 * }
+	 *
 	 * @param bound
 	 * @param length
 	 * @return
@@ -80,8 +102,93 @@ public class Minesweeper {
 	}
 
 	/**
+	 * Decides whether the index should be chosen or not. Its a bit tricky:
+	 * The index should be chosen with the probability of
+	 *   the number of elements to be selected divided by how many elements remains
+	 *
+	 *   length - selectedCounter
+	 *   ------------------------
+	 *        bound - index
+	 *
+	 * @param index
+	 * @param bound
+	 * @param length
+	 * @param randomFloat
+	 * @param selectedCounter
+	 * @return
+	 */
+	public static boolean isIndexChoosedRandomly(final int index, final int bound, final int length, Float randomFloat, AtomicInteger selectedCounter) {
+		if (length == selectedCounter.get()) return false;
+
+		if (randomFloat <= (float)(length - selectedCounter.get()) / (float)(bound - index)) {
+			selectedCounter.incrementAndGet();
+
+			return true;
+		}
+
+		return false;
+	}
+
+	/**
+	 * Get unique random numbers based on another algorithm what I learned in the University (ELTE)
+	 * @param bound
+	 * @param length
+	 * @return
+	 */
+	public static List<Integer> getUniqueRandomsOptimized(final int bound, final int length) {
+		AtomicInteger n = new AtomicInteger(0);
+		Random rnd = new Random();
+
+		return IntStream.range(0, bound).
+				filter(i -> isIndexChoosedRandomly(i, bound, length, rnd.nextFloat(), n)).
+				boxed().collect(Collectors.toList());
+	}
+
+	/**
 	 * Based on the size of the grid and the linear coordinates of the bombs it computes the
-	 * numbers in the bombs-neighbourhood cells
+	 * numbers in the bombs-neighbourhood cells.
+	 *
+	 * The algorithm:
+	 * When there is only one bomb:
+	 * 1100
+	 * *100
+	 * 1100
+	 * 0000
+	 *
+	 * All of the valid neighbourhood cells should contain 1
+	 *
+	 * When there are two bombs far enough from each other:
+	 * 1*10000
+	 * 1110000
+	 * 0000011
+	 * 000001*
+	 * 0000011
+	 *
+	 * Same, all of the valid bomb-neighborhood cells should contain 1
+	 *
+	 * Interesting case: two bombs, which have common neighbourhood cells
+	 *
+	 * 11100
+	 * 1*211
+	 * 112*1
+	 * 00111
+	 *
+	 * Let's "spearate" the bombs:
+	 *
+	 * 11100       00000
+	 * 1*100       00111
+	 * 11100       001*1
+	 * 00000       00111
+	 *
+	 * The algorithm:
+	 * 1) create a list of the neighbourhood of all the bombs
+	 * 2) count the appearance for each cells in this list
+	 * 3) the value of the neighbourhood cells will be its counter
+	 *
+	 * One addition:
+	 * Imagine, that one of the bomb's neighbourhood cell may contain another bomb.
+	 * In this case, this cell should not be in the list, so
+	 * filter out the bomb-cells from the neighbourhood cells.
 	 * @param rows
 	 * @param cols
 	 * @param bombs
--- core/src/test/java/hu/bp/minesweeper/core/MinesweeperTest.java
+++ core/src/test/java/hu/bp/minesweeper/core/MinesweeperTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 import static junit.framework.TestCase.assertTrue;
@@ -85,5 +86,76 @@ public class MinesweeperTest {
 	public void getStringGrid() {
 		MinesweeperData data = MinesweeperTestHelper.getMinesweeperDataFromString(2, 2, "*12 ");
 		assertEquals("*1\n2" + Minesweeper.STR_ZEO, Minesweeper.getStringGrid(data));
+	}
+
+	@Test
+	public void isIndexChoosedRandomly() {
+		AtomicInteger selectedCounter = new AtomicInteger(0);
+		assertTrue(
+				"selects when random is sure",
+				Minesweeper.isIndexChoosedRandomly(0, 10, 1, 0F, selectedCounter)
+		);
+		assertEquals(1, selectedCounter.get());
+
+		selectedCounter = new AtomicInteger(0);
+		assertTrue(
+				"selects all when bound equals length",
+				Minesweeper.isIndexChoosedRandomly(0, 3, 3, 1F, selectedCounter)
+		);
+		assertTrue(
+				"selects all when bound equals length",
+				Minesweeper.isIndexChoosedRandomly(1, 3, 3, 1F, selectedCounter)
+		);
+		assertTrue(
+				"selects all when bound equals length",
+				Minesweeper.isIndexChoosedRandomly(2, 3, 3, 1F, selectedCounter)
+		);
+		assertEquals(3, selectedCounter.get());
+
+		selectedCounter = new AtomicInteger(0);
+		for (int i = 0; i < 5; i++) {
+			Minesweeper.isIndexChoosedRandomly(i, 5, 3, 0.2F, selectedCounter);
+		};
+		assertEquals("it should select exactly length", 3, selectedCounter.get());
+
+	}
+
+
+	@Test
+	public void getUniqueRandomsOptimizedBoundEqualsLength() {
+		List<Integer> randoms = Minesweeper.getUniqueRandomsOptimized(3, 3);
+		Collections.sort(randoms);
+
+		Integer[] sortedRandoms = randoms.toArray(new Integer[0]);
+		assertArrayEquals(new Integer[]{0, 1, 2}, sortedRandoms);
+	}
+
+	@Test
+	public void getUniqueRandomsOptimizedBoundGreaterThanLength() {
+		int bound = 10;
+		int length = 3;
+		List<Integer> randoms = Minesweeper.getUniqueRandomsOptimized(bound, length);
+
+		assertEquals(length, randoms.size());
+
+		Set<Integer> set = new HashSet<>(randoms);
+
+		assertEquals("numbers should unique", randoms.size(), set.size());
+
+		randoms.forEach(r -> assertTrue("numbers should be between 0 and bound", r >= 0 && r < bound));
+	}
+
+	public void performanceTest() {
+		Date start = new Date();
+		for (int i = 0; i < 100000; i++) {
+			Minesweeper.getUniqueRandomsOptimized(100, 10);
+		}
+		System.out.println(new Date().getTime() - start.getTime());
+
+		start = new Date();
+		for (int i = 0; i < 100000; i++) {
+			Minesweeper.getUniqueRandoms(100, 10);
+		}
+		System.out.println(new Date().getTime() - start.getTime());
 	}
 }
